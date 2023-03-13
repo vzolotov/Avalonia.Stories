366ff53bc19d5129d6439eb6b7deef51e1c326da
Slava Zolotov <v.zolotov@outlook.com>
2023-03-14 00:03:55 +0300

    stories preview aligment

--- src/Avalonia.Stories.Demo/MainViewModel.cs
+++ src/Avalonia.Stories.Demo/MainViewModel.cs
@@ -12,6 +12,6 @@ public class MainViewModel : IStoriesViewModel
     {
         Items.Add(new StoriesImageViewModel("image1", new Uri("https://user-images.githubusercontent.com/4672627/152126443-932966cf-57e7-4e77-9be6-62463a66b9f8.png")));
         Items.Add(new StoriesImageViewModel("image2", new Uri("https://github.com/AvaloniaUI/avaloniaui.net/blob/master/assets/showcase/GritGene.png?raw=true")));
-        Items.Add(new StoriesImageViewModel("image3", new Uri("https://github.com/AvaloniaUI/avaloniaui.net/blob/master/assets/showcase/GritGene.png?raw=true")));
+        Items.Add(new StoriesImageViewModel("image3", new Uri("https://github.com/AvaloniaUI/avaloniaui.net/blob/master/assets/showcase/Wasabi.png?raw=true")));
     }
 }
